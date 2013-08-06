cbc85216ace242492531e9b0d71ad83573e0c391
Wang Xuerui <idontknw.wang@gmail.com>
2013-08-06 15:15:47 +0800

    重构配置文件优化 minify 表现; 定义 ui.jq 的依赖

--- js/config.js
+++ js/config.js
@@ -1,34 +1,43 @@
 'use strict';
 
 
-var require = {
-  baseUrl: '//static.jnrain.com/common/js',
-  paths: {
-    'select2': '../standalone/select2/select2'
-  },
-  shim: {
-    'jquery.ui': ['jquery'],
-    'jquery.backstretch': ['jquery'],
-    'jquery.stellar': ['jquery'],
-    'garlic': ['jquery'],
-    'parsley': ['jquery'],
-    'supersized.core': ['jquery'],
-    'select2': ['jquery'],
-    'angular': {
-      deps: ['jquery'],  // force use of real jQuery
-      exports: 'angular'
+var require = (function() {
+  var JQUERY = 'jquery',
+      NG = 'angular';
+
+  return {
+    baseUrl: '//static.jnrain.com/common/js',
+    paths: {
+      'select2': '../standalone/select2/select2'
     },
-    'angular-resource': ['angular'],
-    'angular-sanitize': ['angular'],
-    'angular-dragdrop': {
-      deps: ['jquery.ui', 'angular'],
-      exports: 'jqyoui'
-    },
-    'angular-ui-select2': ['select2', 'angular'],
-    'angular-scrollevents': ['angular'],
-    'stellar.directives': ['angular', 'jquery.stellar']
-  }
-};
+    shim: {
+      // jQuery plugins
+      'jquery.ui': [JQUERY],
+      'jquery.backstretch': [JQUERY],
+      'jquery.stellar': [JQUERY],
+      'garlic': [JQUERY],
+      'parsley': [JQUERY],
+      'supersized.core': [JQUERY],
+      'select2': [JQUERY],
+
+      // AngularJS components
+      NG: {
+        deps: [JQUERY],  // force use of real jQuery
+        exports: NG
+      },
+      'angular-resource': [NG],
+      'angular-sanitize': [NG],
+      'angular-dragdrop': {
+        deps: ['jquery.ui', NG],
+        exports: 'jqyoui'
+      },
+      'angular-ui-select2': ['select2', NG],
+      'angular-scrollevents': [NG],
+      'stellar.directives': [NG, 'jquery.stellar'],
+      'ui.jq': [JQUERY, NG]
+    }
+  };
+})();
 
 
 // vim:set ai et ts=2 sw=2 sts=2 fenc=utf-8:
