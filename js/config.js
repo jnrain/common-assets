15beb1f95fa20d746bc8949d9e2cb8700445a640
Wang Xuerui <idontknw.wang@gmail.com>
2013-08-08 16:17:46 +0800

    fix config

--- js/config.js
+++ js/config.js
@@ -3,41 +3,42 @@
 
 var require = (function() {
   var JQUERY = 'jquery',
-      NG = 'angular';
+      NG = 'angular',
+      result = {
+          baseUrl: '//static.jnrain.com/common/js',
+          paths: {
+            'select2': '../standalone/select2/select2'
+          },
+          shim: {
+            // jQuery plugins
+            'jquery.ui': [JQUERY],
+            'jquery.backstretch': [JQUERY],
+            'jquery.stellar': [JQUERY],
+            'garlic': [JQUERY],
+            'parsley': [JQUERY],
+            'supersized.core': [JQUERY],
+            'select2': [JQUERY],
+            'waypoints': [JQUERY],
 
-  return {
-    baseUrl: '//static.jnrain.com/common/js',
-    paths: {
-      'select2': '../standalone/select2/select2'
-    },
-    shim: {
-      // jQuery plugins
-      'jquery.ui': [JQUERY],
-      'jquery.backstretch': [JQUERY],
-      'jquery.stellar': [JQUERY],
-      'garlic': [JQUERY],
-      'parsley': [JQUERY],
-      'supersized.core': [JQUERY],
-      'select2': [JQUERY],
-      'waypoints': [JQUERY],
+            // AngularJS components
+            NG: {
+              deps: [JQUERY],  // force use of real jQuery
+              exports: NG
+            },
+            'angular-resource': [NG],
+            'angular-sanitize': [NG],
+            'angular-dragdrop': {
+              deps: ['jquery.ui', NG],
+              exports: 'jqyoui'
+            },
+            'angular-ui-select2': ['select2', NG],
+            'angular-scrollevents': [NG],
+            'stellar.directives': [NG, 'jquery.stellar'],
+            'ui.jq': [JQUERY, NG]
+          }
+        };
 
-      // AngularJS components
-      NG: {
-        deps: [JQUERY],  // force use of real jQuery
-        exports: NG
-      },
-      'angular-resource': [NG],
-      'angular-sanitize': [NG],
-      'angular-dragdrop': {
-        deps: ['jquery.ui', NG],
-        exports: 'jqyoui'
-      },
-      'angular-ui-select2': ['select2', NG],
-      'angular-scrollevents': [NG],
-      'stellar.directives': [NG, 'jquery.stellar'],
-      'ui.jq': [JQUERY, NG]
-    }
-  };
+    return result;
 })();
 
 
